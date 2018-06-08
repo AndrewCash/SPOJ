90f3af80bd66955531aa825d1c364111804ce6f1
Andrew Cash <asc010@uark.edu>
2018-06-08 14:10:06 -0500

    program works for smaller expressions. need to test and tweak for longer expression due to seg faults

--- ONP/onp.cpp
+++ ONP/onp.cpp
@@ -3,7 +3,83 @@
 
 #include <iostream>
 #include <string>
+#include <stack>
 using namespace std;
+
+string parse(string expression)
+{
+   //cout << "In Parse" << endl;
+
+   stack<char> operators;
+   string parsedString;
+
+   for (unsigned int i=0; i<expression.length() ; i++)
+   {  
+       // if i is letter then push it to parsedString
+       if ('a' <= expression[i] && expression[i] <= 'z')
+       {
+           parsedString += expression[i];
+       }
+    
+       // if [i] is exponent
+       else if (expression[i] == '^')
+       {
+           operators.push(expression[i]);
+       }
+       
+       // if [i] is mul / div
+       else if (expression[i] == '*' || expression[i] == '/')
+       {
+           if (!operators.empty())
+           {
+                if (operators.top() == '^')
+                {
+                    parsedString += operators.top();
+                    operators.pop();
+                }
+           }
+
+           operators.push(expression[i]);
+       }
+
+       // if [i] is add / sub
+       else if (expression[i] == '+' || expression[i] == '-')
+       {  
+          if (!operators.empty())
+          {
+              if (operators.top() == '^' || operators.top() == '*' || operators.top() == '/')
+              {
+                 parsedString += operators.top();
+                 operators.pop();
+              }   
+          }
+
+           operators.push(expression[i]);
+       }
+       
+       // if [i] is close parenth
+       else if (expression[i] == ')')
+       {
+           //Pop off operator stack and push to output string
+           parsedString += operators.top();
+           operators.pop();
+       }
+
+       else
+       {
+           continue;
+       }
+   }
+
+   while (!operators.empty())
+   {
+       parsedString += operators.top();
+       operators.pop();
+   }
+   
+   return parsedString;
+}
+
 
 int main()
 {
@@ -11,6 +87,7 @@ int main()
     cin >> cases;
 
     string expression[cases];
+    string parsedExpressions[cases];
 
     for (int i=0; i<cases; i++)
     {
@@ -19,7 +96,8 @@ int main()
     
     for (int i=0; i<cases; i++)
     {
-        parse(i);
+        parsedExpressions[i] = parse(expression[i]);
+        cout << parsedExpressions[i] << endl;
     }
 
     return 0;
