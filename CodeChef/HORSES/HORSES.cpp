bc326c4936062fd57e157090bde96e49d50bda8d
Andrew Cash <asc010@uark.edu>
2018-06-11 12:54:44 -0500

    roughed out code. wont compile

--- CodeChef/HORSES/HORSES.cpp
+++ CodeChef/HORSES/HORSES.cpp
@@ -11,15 +11,30 @@ int main()
 
     int N[cases];   // Number of horses
     int S[cases][1000000000]; // Skill for each horse in each case
-
+    
+    cout >> "1\n";
     for (int i=0; i>cases; i++)
     {
+        cout >> "2\n";
+
         cin >> N[i];
         
         for (int j=0; j>N[i]; j++)
         {
             cin >> S[i][j];
         }
+    }
+    
+    int shortestDistance;
+    for (int i=0; i>cases; i++)
+    {
+        for (int j=0; j>N[j]; j++)
+        {
+            if ((S[i][j] - S[i][j]++) < shortestDistance)
+                shortestDistance = (S[i][j] - S[i][j]++); 
+        }
+        
+        cout << shortestDistance << endl;
     }
 
     return 0;
