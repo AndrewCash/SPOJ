3187d5dab61e945393fe72895330080e084fbaac
Andrew Cash <andrewcash@Andrews-MacBook-Pro.local>
2018-06-27 12:34:36 -0500

    input variables

--- CodeChef/INOI Practice Contest/INOI1402/FreeTicket.cpp
+++ CodeChef/INOI Practice Contest/INOI1402/FreeTicket.cpp
@@ -1,8 +1,14 @@
 // Andrew Cash
 // https://www.codechef.com/INOIPRAC/problems/INOI1402
 
-#include <stdio.h>
+#include <iostream>
 using namespace std;
 
 int main() {
+    int C, F;
+    cin >> C >> F;
+    
+    cout << C << F << endl;
+
+    return 0;
 }
