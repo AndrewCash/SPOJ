77775dd6f1b7f08720960fa4540dcca72ae62e3d
AndrewCash <asc010@uark.edu>
2018-06-29 13:42:39 -0500

    Compiles

--- CodeChef/INOI Practice Contest/INOI1402/FreeTicket.cpp
+++ CodeChef/INOI Practice Contest/INOI1402/FreeTicket.cpp
@@ -4,24 +4,28 @@
 #include <iostream>
 using namespace std;
 
-class Flight {
+// Flight node. 
+class Flight { 
     public:
-        // Getters
-        // Setters
-
-    private:
+        Flight();
+        void insertNode(int X, int Y);
         
-}
+    private:
+        int price;
+        
+};
 
 int main() {
     int C, F;       // C = # of cities
     cin >> C >> F;  // F = # of cities connected
     
+
+    // Add flights to map
     int x, y, p;
     for (int i=0; i<F+1; i++) {
-        cin x; // Ordered pair of cities 
-        cin y; // connected by flights.
-        cin p; // Price of flight
+        cin >> x; // Ordered pair of cities 
+        cin >> y; // connected by flights.
+        cin >> p; // Price of flight
     }
 
     return 0;
