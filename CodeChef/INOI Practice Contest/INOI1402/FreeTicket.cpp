eff7a061bd6d4a0f9568b5d68d937195fca7a14d
Andrew Cash <cashandrew41@gmail.com>
2018-07-04 00:05:41 -0500

    Changd STL packaged called. Now getting output!
    Runs on my macbook pro. Not sure about turing. 
    
    Example output:
    City 0 has a connecting flight with
    	city 2 with price = 10
    	city 3 with price = 24
    
    City 1 has a connecting flight with
    	city 3 with price = 2
    	city 4 with price = 15
    
    City 2 has a connecting flight with
    	city 0 with price = 10
    	city 4 with price = 7
    	city 4 with price = 7
    
    City 3 has a connecting flight with
    	city 0 with price = 24
    	city 1 with price = 2

--- CodeChef/INOI Practice Contest/INOI1402/FreeTicket.cpp
+++ CodeChef/INOI Practice Contest/INOI1402/FreeTicket.cpp
@@ -1,15 +1,16 @@
 // Andrew Cash
 // https://www.codechef.com/INOIPRAC/problems/INOI1402
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 // Add edge to graph
 //  u = vertex represented by index of vector
 //  v = vertex connected to index vertex
 //  price = weight of edge
-void addFlight(vector<pair<int, int> > AdjMatrix[], int u, int v, int price) 
-{ 
+void addFlight(vector<pair<int, int> > AdjMatrix[], int u, int v, int price)
+{
     AdjMatrix[u].push_back(make_pair(v, price));
     AdjMatrix[v].push_back(make_pair(u, price));
 }
@@ -33,7 +34,7 @@ void printGraph(vector<pair<int, int> > AdjMatrix[], int Cities)
 int main() {
     int C, F;       // C = # of cities / verticies
     cin >> C >> F;  // F = # of pairs of cities connected / edges
- 
+
     // Vector contains adjacency matrix for graph.
     // Vertex 1 = index of vector
     // Vertex 2 = first int
@@ -43,13 +44,13 @@ int main() {
     // Add flights to map
     int x, y, p;
     for (int i=0; i<F+1; i++) {
-        cin >> x; // Ordered pair of cities 
+        cin >> x; // Ordered pair of cities
         cin >> y; // connected by flights.
         cin >> p; // Price of flight.
 
         addFlight(AdjMatrix, x-1, y, p); // Note: x-1 because x is zero indexed. Avoids segfault
     }
-    
+
     printGraph(AdjMatrix, C);
 
     return 0;
