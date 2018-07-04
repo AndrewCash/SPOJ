3aac629b86c544159d75e006af8b173dfd61ab92
Andrew Cash <cashandrew41@gmail.com>
2018-07-04 00:39:08 -0500

    More comments. Output context.

--- CodeChef/INOI Practice Contest/INOI1402/FreeTicket.cpp
+++ CodeChef/INOI Practice Contest/INOI1402/FreeTicket.cpp
@@ -1,6 +1,32 @@
 // Andrew Cash
 // https://www.codechef.com/INOIPRAC/problems/INOI1402
 
+// Goal:
+/*
+
+Some dude has won the contest and he is entitled to a free ticket
+between any two destinations served by the airline. All cities served
+by Drongo Airlines can be reached from each other by some sequence of
+connecting flights. Nikhil is allowed to take as many connecting flights
+as needed, but he must take the cheapest route between his chosen destinations.
+
+Each direct flight between two cities has a fixed price. All pairs of
+cities connected by direct flights have flights in both directions
+and the price is the same in either direction. The price for a
+sequence of connecting flights is the sum of the prices of the direct
+flights along the route.
+
+We would like to maximize the value of the prize, so we would like
+to choose a pair of cities on the network for which the cost of
+the cheapest route is as high as possible.
+
+Output:
+The output consists of a single integer,
+the maximum cost among the cheapest routes
+between all pairs of cities across the airline’s network.
+
+*/
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -53,6 +79,9 @@ int main() {
     }
 
     printGraph(AdjMatrix, C);
+
+
+
 
     return 0;
 }
