fb13741f4fff2df47c6393dc81f7d399af126d8d
AndrewCash <asc010@uark.edu>
2018-07-09 14:32:35 -0500

    Initial commit

---
+++ CodeChef/CLEANUP/CLEANUP.cpp
@@ -0,0 +1,22 @@
+// Andrew Cash
+// https://www.codechef.com/problems/CLEANUP
+
+#include <iostream>
+using namespace std;
+
+void cleanup()
+{
+    int m, n;
+}
+
+int main()
+{
+    int testcase;
+    cin >> testcase;
+
+    for (int i=0; i<testcase; i++)
+    {
+        cleanup();    
+    }
+    return 0;
+}
