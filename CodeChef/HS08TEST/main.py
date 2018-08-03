c4b6a6e642dbdf7ef8f0c58970063f2ebf6daa7d
AndrewCash <asc010@uark.edu>
2018-08-03 12:06:45 -0500

    output balance if incorrect input is given

--- CodeChef/HS08TEST/main.py
+++ CodeChef/HS08TEST/main.py
@@ -7,7 +7,7 @@ var1, var2 = raw_input().split()
 withdrawal, balance = [float(var1), float(var2)]
 
 if withdrawal % 5 != 0:
-    exit (0)
+    print balance
 
 if ((balance + 0.5) > withdrawal):
     balance = balance - withdrawal - 0.5
