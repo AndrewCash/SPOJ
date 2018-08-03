79b7ae2c6828e9e29924cc74054f5eb8ad233b0a
AndrewCash <asc010@uark.edu>
2018-08-03 12:35:14 -0500

    Took care of exceptions when withdrawal isnt divisible by 5 and not more than balance

--- CodeChef/HS08TEST/main.py
+++ CodeChef/HS08TEST/main.py
@@ -6,11 +6,15 @@ from decimal import Decimal, ROUND_HALF_UP
 var1, var2 = raw_input().split()
 withdrawal, balance = [float(var1), float(var2)]
 
-if withdrawal % 5 != 0:
-    print balance
+if (withdrawal % 5 != 0) or (withdrawal > balance):
+    #print balance
+    our_value = Decimal(balance)
+    output = Decimal(our_value.quantize(Decimal('.01'), rounding=ROUND_HALF_UP))
+    print output
 
-if ((balance + 0.5) > withdrawal):
+elif ((balance + 0.5) > withdrawal):
     balance = balance - withdrawal - 0.5
     our_value = Decimal(balance)
     output = Decimal(our_value.quantize(Decimal('.01'), rounding=ROUND_HALF_UP))
     print output
+
