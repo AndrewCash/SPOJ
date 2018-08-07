a14576e3ab7fb1fcee1975fb0f005d3756e600ea
Andrew Cash <cashandrew41@gmail.com>
2018-08-06 20:03:29 -0500

    Basic problem. Easy

---
+++ SPOJ/TESTINT/testint.cpp
@@ -0,0 +1,14 @@
+// Andrew Cash
+// https://www.spoj.com/problems/TESTINT/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int one, two;
+
+    cin >> one >> two;
+    cout << one + two << endl;
+
+    return 0;
+}
