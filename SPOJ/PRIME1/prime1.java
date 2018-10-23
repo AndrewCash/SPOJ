23ba9f5c52e9a3ec01afd7c82a89b3c7a705d27f
Andrew Cash <cashandrew41@gmail.com>
2018-10-23 16:05:11 -0500

    Tinkered in java

---
+++ SPOJ/PRIME1/prime1.java
@@ -0,0 +1,16 @@
+// Andrew Cash
+// Fall 2018
+
+import java.util.ArrayList;
+import java.util.Sacnner;
+
+class Main {
+    ArrayList<int> low;
+    ArrayList<int> high;
+    Scanner S = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        low = new ArrayList<int>();
+    }
+}
